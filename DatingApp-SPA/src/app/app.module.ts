aae437d8dd64913cdd1e54682f8ef3c2c096b735
Cedric Memel <cedric.memel@outlook.com>
2018-11-22 19:06:04 +0000

    end of section 5

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
-import { HttpClientModule } from "@angular/common/http";
+import { HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http";
 import { FormsModule } from "@angular/forms";
 
 import { AppComponent } from "./app.component";
@@ -8,11 +8,17 @@ import { NavComponent } from "./nav/nav.component";
 import { AuthService } from "./_services/Auth.service";
 import { HomeComponent } from "./home/home.component";
 import { RegisterComponent } from "./register/register.component";
+import {
+  ErrorInterceptor,
+  ErrorInterceptorProvider
+} from "./_services/error.interceptor";
 
 @NgModule({
   declarations: [AppComponent, NavComponent, HomeComponent, RegisterComponent],
+
   imports: [BrowserModule, HttpClientModule, FormsModule],
-  providers: [AuthService],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+
+  providers: [AuthService, ErrorInterceptorProvider]
 })
 export class AppModule {}
--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -18,7 +18,7 @@ export class NavComponent implements OnInit {
         console.log("Logged in succesfully");
       },
       error => {
-        console.log("Login attempt failed");
+        console.log(error);
       }
     );
   }
