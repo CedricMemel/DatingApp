bd4a4ce2f4be4f094254c49c42f1ef2bc3a32caa
Cedric Memel <cedric.memel@outlook.com>
2018-12-11 20:50:27 +0000

    seeding data to the database and creation of Dtos

---
+++ DatingApp.API/Controllers/UsersController.cs
@@ -0,0 +1,48 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using AutoMapper;
+using DatingApp.API.Data;
+using DatingApp.API.DTOs;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DatingApp.API.Controllers
+{
+    [Authorize]
+    [Route("api/[Controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly IDatingRepository _repo;
+        private readonly IMapper _mapper;
+        public UsersController(IDatingRepository repo, IMapper mapper)
+        {
+            _mapper = mapper;
+            _repo = repo;
+
+        }
+        [HttpGet("GetUsers")]
+        public async Task<IActionResult> GetUsers()
+        {
+            var users = await _repo.GetUsers();
+            var usersToReturn = _mapper.Map<IEnumerable<UserForListDto>>(users);
+
+            return Ok(usersToReturn);
+        }
+
+        [HttpGet("Getuser/{id}")]
+        public async Task<IActionResult> Getuser(int id)
+        {
+            var user = await _repo.GetUser(id);
+            var userToreturn = _mapper.Map<UserForDetailedDto>(user);
+
+            return Ok(user);
+        }
+
+
+
+
+
+
+    }
+}
